df79ce9d1e047d67f4d5ab811b36bcdd000294d9
sheiling <863583026@qq.com>
2020-12-09 15:13:12 +0800

    image test

--- ScaleImage/main.cpp
+++ ScaleImage/main.cpp
@@ -3,12 +3,12 @@
 
 int main()
 {
-	cv::Mat image = cv::imread("", cv::IMREAD_GRAYSCALE);
+	cv::Mat image = cv::imread("test.bmp", cv::IMREAD_GRAYSCALE);
 	if (image.empty())
 		return -1;
 
 	// 对图像的灰度进行拉伸
-	ScaleImage::ScaleRange(image, 30, 100, 0, 255);
+	cv::Mat matResult = ScaleImage::ScaleRange(image, 34, 60, 0, 255);
 
 	return -1;
 }
